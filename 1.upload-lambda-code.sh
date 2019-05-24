3e170a8008eb6c6823e02ce7179490609f7a5f30
Able710 <craig.lewis@contactpartners.com>
2019-05-24 23:34:15 +0100

    Updated the Bash Script to work with Git Bash

--- 1.upload-lambda-code.sh
+++ 1.upload-lambda-code.sh
@@ -3,6 +3,8 @@
 # bash script that will zip up a deployment package, upload a lambda function
 # to a specified s3 bucket. The script will then deploy the function to lambda (node version)
 
+echo -n "Enter the name of the awscli profile you want to use: "
+read AWSProfile
 echo -n "Enter the name of the files you wish to zip (eg. lambdaFunction.js node_modules): "
 read FilesToBeZipped
 echo -n "Enter the name of the output zip file (eg. lambdaFunction): "
@@ -16,9 +18,9 @@ read BucketName
 #echo -n "Enter the ARN of the role you wish to implement: "
 #read Role
 
-zip -r "$ZipFileName.zip" $FilesToBeZipped
+git archive -o "$ZipFileName.zip" $FilesToBeZipped
 
-aws-able s3api put-object --bucket $BucketName --key "./$ZipFileName.zip" --body "./$ZipFileName.zip"
+aws s3api put-object --bucket $BucketName --key "./$ZipFileName.zip" --body "./$ZipFileName.zip" --profile $AWSProfile
 
 #aws lambda create-function --function-name $FunctionName --runtime nodejs \
 #--role $Role --handler "$ZipFileName.handler" \
