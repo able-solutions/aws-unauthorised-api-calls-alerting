1b64b159391f3c41facaf09f7e02c6d89d0a6f80
Able710 <craig.lewis@contactpartners.com>
2019-05-25 14:11:40 +0100

    Removed the update.sh script and removed the bash script description for the deploy.sh script

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# bash script that will zip up a deployment package, upload a lambda function
-# to a specified s3 bucket. The script will then deploy the function to lambda (node version)
-
 echo -n "Enter the name of the awscli profile you want to use: "
 read AWSProfile
 #echo -n "Enter the name of the files you wish to zip (eg. lambdaFunction.js node_modules): "
